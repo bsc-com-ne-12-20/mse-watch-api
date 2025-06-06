92b178b04de4838b52d3a059cdd9c1020a41dda2
bsc-com-ne-12-20 <bsc-com-ne-12-20@unima.ac.mw>
2025-06-06 11:26:43 +0200

    admin site reg

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Stock, StockPrice, StockPriceHistory, Company, HistoricalPrice
+
+admin.site.register(Stock)
+admin.site.register(StockPrice)
+admin.site.register(StockPriceHistory)
+admin.site.register(Company)
+admin.site.register(HistoricalPrice)
+
 
 # Register your models here.
